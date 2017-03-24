845eba03f4597f140f23b61bbcfa395835e3cca3
Vladimir <alu8bit@users.noreply.github.com>
2017-03-24 14:41:42 +0300

    Add script for user software installation

---
+++ soft/install_core_software.sh
@@ -0,0 +1,14 @@
+!#/bin/bash
+
+# core software installation
+
+#package: kate
+#description: Advanced Text Editor
+#command: sudo apt-get install kate
+
+#package: htop
+#description: Interactive processes viewer
+#command: sudo apt-get install htop
+
+# install all
+sudo apt-get update; sudo apt-get -y install kate htop
